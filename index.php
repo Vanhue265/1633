abbce08a1150f93224fd77e094eb102ab9b22db4
Kienfpt202 <94580960+Kienfpt202@users.noreply.github.com>
2023-10-31 20:29:16 +0700

    code css home, login and register

--- index.php
+++ index.php
@@ -2,14 +2,22 @@
 <html>
 <head>
     <title>Trang Chủ</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resource/css/style.css">
 </head>
 <body>
-    <h1>Trang Chủ</h1>
-    <p>Chào mừng đến với trang web dịch vụ chăm sóc thú nuôi.</p>
-    <p>Đăng nhập hoặc đăng ký để truy cập các dịch vụ của chúng tôi.</p>
-    <ul>
-        <li><a href="login.php">Đăng nhập</a></li>
-        <li><a href="register.php">Đăng ký</a></li>
-    </ul>
+    <div class="container">
+        <h1>Trang Chủ</h1>
+        <p>Chào mừng đến với trang web dịch vụ chăm sóc thú nuôi.</p>
+        <p>Đăng nhập hoặc đăng ký để truy cập các dịch vụ của chúng tôi.</p>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Đăng nhập</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">Đăng ký</a>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once 'connect.php'; // Sử dụng tệp connect.php để kết nối đến cơ sở dữ liệu
+require_once 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($user) {
         $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_role'] = $user['is_admin'] ? "admin" : "customer"; // Sử dụng "is_admin" để xác định vai trò
+        $_SESSION['user_role'] = $user['is_admin'] ? "admin" : "customer";
 
         if ($user['is_admin']) {
             header("Location: admin_dashboard.php");
@@ -33,18 +33,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Đăng nhập</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Đăng nhập</h1>
-    <?php if (isset($error_message)) : ?>
-        <p><?php echo $error_message; ?></p>
-    <?php endif; ?>
-    <form method="POST">
-        <label for="username">Tên đăng nhập:</label>
-        <input type="text" name="username" required>
-        <label for="password">Mật khẩu:</label>
-        <input type="password" name="password" required>
-        <button type="submit">Đăng nhập</button>
-    </form>
+    <div class="container">
+        <h1>Đăng nhập</h1>
+        <?php if (isset($error_message)) : ?>
+            <p class="error"><?php echo $error_message; ?></p>
+        <?php endif; ?>
+        <form method="POST">
+            <div class="mb-3">
+                <label for="username" class="form-label">Tên đăng nhập:</label>
+                <input type="text" class="form-control" name="username" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Mật khẩu:</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Đăng nhập</button>
+        </form>
+    </div>
 </body>
 </html>
